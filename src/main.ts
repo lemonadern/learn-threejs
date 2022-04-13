58e9d6cdf1977d227d220a238338ff815b63ab44
lemonadern <taishi.develop@gmail.com>
2022-04-13 21:16:43 +0900

    use OrbitControls

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 window.addEventListener('DOMContentLoaded', init);
 
@@ -7,7 +8,6 @@ function init() {
   // サイズを指定
   const width = 960;
   const height = 540;
-  let rot = 0;
 
   const canvasElement = document.getElementById('myCanvas') as HTMLCanvasElement;
   if(canvasElement == null) {
@@ -26,72 +26,26 @@ function init() {
 
   // カメラを作成
   const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-  camera.position.set(0, 0, +1000);
+  camera.position.set(0, 0, 1000);
 
-  // 平行光源を作成
-  const directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.position.set(1, 1, 1);
-  scene.add(directionalLight);
+  // カメラコントローラーを作成
+  const controls = new OrbitControls(camera, canvasElement);
+  // 滑らかにカメラコントローラーを制御する
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.2;
 
-  // マテリアルを作成
-  const material = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load('/assets/earthmap1k.jpg'),
-    side: THREE.DoubleSide,
-  });
-
-  // 球体の形状を作成します
-  const geometry = new THREE.SphereGeometry(300, 30, 30);
   // 形状とマテリアルからメッシュを作成します
-  const earthMesh = new THREE.Mesh(geometry, material);
-  // シーンにメッシュを追加します
-  scene.add(earthMesh);
-
-  // 星屑を作成します (カメラの動きをわかりやすくするため)
-  createStarField();
-
-  /** 星屑を作成します */
-  function createStarField() {
-    // 頂点情報
-    const vertices = [];
-    for (let i = 0; i < 1000; i++) {
-      const x = 3000 * (Math.random() - 0.5);
-      const y = 3000 * (Math.random() - 0.5);
-      const z = 3000 * (Math.random() - 0.5);
-
-      vertices.push(x, y, z);
-    }
-
-    // 形状データを作成
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-
-    // マテリアルを作成
-    const material = new THREE.PointsMaterial({
-      size: 10,
-      color: 0xffffff,
-    });
-
-    // 物体を作成
-    const mesh = new THREE.Points(geometry, material);
-    scene.add(mesh);
-  }
+  const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(300, 300, 300),
+    new THREE.MeshNormalMaterial());
+  scene.add(mesh);
 
   tick();
 
   // 毎フレーム時に実行されるループイベントです
   function tick() {
-    rot += 0.5; // 毎フレーム角度を0.5度ずつ足していく
-    // ラジアンに変換する
-    const radian = (rot * Math.PI) / 180;
-    // 角度に応じてカメラの位置を設定
-    camera.position.x = 1000 * Math.sin(radian);
-    camera.position.z = 1000 * Math.cos(radian);
-    // 原点方向を見つめる
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
-
     // レンダリング
     renderer.render(scene, camera);
-
     requestAnimationFrame(tick);
   }
 }
