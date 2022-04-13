3dd4b5c0e205577a3dbe0e40393118818c971a80
lemonadern <taishi.develop@gmail.com>
2022-04-13 14:32:42 +0900

    run rotating cube

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
-import './style.css'
+import * as THREE from 'three'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+window.addEventListener('DOMContentLoaded', init);
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function init() {
+
+  // サイズを指定
+  const width = 960;
+  const height = 540;
+
+  const canvasElement = document.getElementById('myCanvas') as HTMLCanvasElement;
+  if(canvasElement == null) {
+    return
+  }
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvasElement,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, width / height);
+  camera.position.set(0, 0, +1000);
+
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(400, 400, 400);
+  const material = new THREE.MeshNormalMaterial();
+  const box = new THREE.Mesh(geometry, material);
+  scene.add(box);
+
+  tick();
+
+  // 毎フレーム時に実行されるループイベントです
+  function tick() {
+    box.rotation.y += 0.01;
+    renderer.render(scene, camera); // レンダリング
+
+    requestAnimationFrame(tick);
+  }
+}
